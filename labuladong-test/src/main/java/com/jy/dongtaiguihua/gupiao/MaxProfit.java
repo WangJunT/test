f0d1e6ba73a2de3a4179a336620a7ac559e6079d
86159 <1365644603@qq.com>
2021-08-19 22:05:27 +0800

    接上一次提交

--- labuladong-test/src/main/java/com/jy/dongtaiguihua/gupiao/MaxProfit.java
+++ labuladong-test/src/main/java/com/jy/dongtaiguihua/gupiao/MaxProfit.java
@@ -24,7 +24,8 @@ package com.jy.dongtaiguihua.gupiao;
 public class MaxProfit {
     //
     public static void main(String[] args) {
-
+        maxProfit(new int[]{3, 3, 5, 0, 0, 3, 1, 4});
+        System.out.println(1);
     }
 
     /**
@@ -75,4 +76,24 @@ public class MaxProfit {
 //        }
 //    }
 //        return dp[prices.length - 1][k][0];
+
+    //https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-iii/submissions/
+    public static int maxProfit(int[] prices) {
+        int k = 2;
+        int[][][] dp = new int[prices.length][k + 1][2];
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = k; j >= 1; j--) {
+                if (i == 0) {
+                    dp[i][j][0] = 0;
+                    dp[i][j][1] = -prices[i];
+                    continue;
+                }
+                dp[i][j][0] = Math.max(dp[i - 1][j][0], dp[i - 1][j][1] + prices[i]);
+
+                dp[i][j][1] = Math.max(dp[i - 1][j][1], dp[i - 1][j - 1][0] - prices[i]);
+            }
+        }
+        return dp[prices.length - 1][k][0];
+    }
+
 }

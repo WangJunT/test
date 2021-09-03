91ca4c9c4516684cb08c96ccee607aa1b74ee430
汪俊 <1365644603@qq.com>
2021-09-03 14:14:12 +0800

    二数之和fix

--- labuladong-test/src/main/java/com/jy/dongtaiguihua/shuangzhizhen/TwoSum.java
+++ labuladong-test/src/main/java/com/jy/dongtaiguihua/shuangzhizhen/TwoSum.java
@@ -1,31 +1,43 @@
 package com.jy.dongtaiguihua.shuangzhizhen;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 双指针技巧：
- *      快慢指针：链表的环，链表的环的位置，删除链表第n个节点
- *      左右指针：二分查找，两数之和，反转字符
+ * 快慢指针：链表的环，链表的环的位置，删除链表第n个节点
+ * 左右指针：二分查找，两数之和（已排序），反转字符
  */
 public class TwoSum {
-    public int[] twoSum(int[] numbers, int target) {
-        int left = 0;
-        int right = numbers.length - 1;
-
-        while (left <= right) {
-            if (numbers[left] + numbers[right] == target) {
-                break;
-            } else if (numbers[left] + numbers[right]  > target) {
-                right--;
-            } else if (numbers[left] + numbers[right] < target) {
-                 left++;
+    //未排序
+    public int[] twoSum(int[] nums, int target) {
+        int[] intaaa = new int[2];
+        Map<Integer, Integer> map = new HashMap();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target-nums[i])) {
+                intaaa[0] = map.get(target-nums[i]);
+                intaaa[1] = i;
+                return intaaa;
+            } else {
+                map.put(nums[i],i);
             }
         }
-        int[] res = new int[2];
-        res[0] = left + 1;
-        res[1] = right + 1;
-        return res;
+
+        return intaaa;
     }
 
     public static void main(String[] args) {
-
+        int[] numbers = {1, 3};
+        int target = 9;
+        int[] intaaa = new int[2];
+        Map<Integer, Integer> map = new HashMap();
+        for (int i = 0; i < numbers.length; i++) {
+            if (map.containsKey(target-numbers[i])) {
+                intaaa[0] = map.get(target-numbers[i]);
+                intaaa[1] = i;
+                return;
+            }
+        }
     }
 }
